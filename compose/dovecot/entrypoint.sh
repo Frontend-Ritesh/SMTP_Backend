8ab921532cb531daa2d8f453d7d89609e392ce78
Aniket Kolamkar <71761243+Aniket-kolamkar@users.noreply.github.com>
2026-06-15 12:13:30 +0530

    feat: add dovecot entrypoint script for configuration injection, mail-crypt key generation, and master user setup

--- compose/dovecot/entrypoint.sh
+++ compose/dovecot/entrypoint.sh
@@ -2,13 +2,6 @@
 set -e
 sed -i "s|__SQL_RO_USER__|${SQL_RO_USER}|; s|__SQL_RO_PASSWORD__|${SQL_RO_PASSWORD}|; s|__POSTGRES_DB__|${POSTGRES_DB}|" \
     /etc/dovecot/dovecot-sql.conf.ext
-# Master user password file (webmail service credential).
-printf '%s:{ARGON2ID}%s\n' "${DOVECOT_MASTER_USER}" \
-  "$(doveadm pw -s ARGON2ID -p "${DOVECOT_MASTER_PASSWORD}" | sed 's/{ARGON2ID}//')" \
-  > /etc/dovecot/master-users
-chown dovecot:dovecot /etc/dovecot/master-users
-chmod 600 /etc/dovecot/master-users
-
 # Generate EC keypair for mail-crypt if not exists
 if [ ! -f /var/vmail/ecprivkey.pem ]; then
   echo "Generating mail-crypt key pair..."
@@ -25,6 +18,13 @@ if [ ! -f /var/vmail/ecpubkey.pem ]; then
   chown vmail:vmail /var/vmail/ecpubkey.pem
   chmod 644 /var/vmail/ecpubkey.pem
 fi
+
+# Master user password file (webmail service credential).
+printf '%s:{ARGON2ID}%s\n' "${DOVECOT_MASTER_USER}" \
+  "$(doveadm pw -s ARGON2ID -p "${DOVECOT_MASTER_PASSWORD}" | sed 's/{ARGON2ID}//')" \
+  > /etc/dovecot/master-users
+chown dovecot:dovecot /etc/dovecot/master-users
+chmod 600 /etc/dovecot/master-users
 
 # Dynamically set SSL certificate paths based on MAIL_HOSTNAME
 if [ -n "${MAIL_HOSTNAME}" ]; then
